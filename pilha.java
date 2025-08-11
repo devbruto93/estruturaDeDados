20ee80669f9a20c65737d55c477bff51fb5ba0b7
devbruto93 <douglas.modest.2013@gmail.com>
2025-08-11 14:50:36 -0300

    implementação da pilha(stack)

---
+++ pilha.java
@@ -0,0 +1,34 @@
+public class pilha {
+  private int valores[];
+  private int topo;
+
+  public pilha(){
+    valores = new int[10];
+    topo = -1; /*indica que a pilha esta vazia, 
+    no Java a posiçao inicial de um vetor é 0,
+     ao atribuir o valor -1 indica a posiça abaixo da posição 0.*/
+  }
+
+  public void push(int elemento){
+    topo = topo ++; //adciona +1 elemento na pilha
+    valores[topo] = elemento;
+  }
+
+  public boolean isEmpty(){
+    return (topo == -1); /*a aplicação fara o teste se esta vazio
+     ou nao pra so assim fazer um pop */
+  }
+
+  public boolean isFull(){
+    return (topo == 9); /*verifica se a pilha esta cheia antes de
+     fazer um push */
+  }
+
+  public int pop(){
+    int elem = valores[topo];
+    topo --; // remove um elemento da pilha na ordem correta
+    return elem;
+  }
+}
+
+//pilha em ingles == stack
